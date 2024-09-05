cf5df95fe3fff5289052342a73a5a792a5835201
Christian Caracach <christiancaracach@gmail.com>
2024-09-04 23:15:22 -0300

    agregue una funcion nueva

--- maquina_cremallera.py
+++ maquina_cremallera.py
@@ -1,3 +1,7 @@
 class MaquinaCremallera:
     def __init__(self):
         self.cremalleras = 0
+
+    def tejerCinta(self, cantidad):
+        self.cintas_tejidas += cantidad
+        print(f"{cantidad} cintas tejidas correctamente.")
