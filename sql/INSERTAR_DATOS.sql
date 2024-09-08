e8875c64d1cfdf4932e286a4ab8773a2189416a0
Christian Caracach <christiancaracach@gmail.com>
2024-09-08 09:53:13 -0300

    agregue fecha a las sentencias insert

--- sql/INSERTAR_DATOS.sql
+++ sql/INSERTAR_DATOS.sql
@@ -12,37 +12,42 @@ INSERT INTO HistorialProduccion (
     cantidad_cremalleras_fabricadas,
     color_teñido,
     cantidad_cremalleras_teñidas,
-    cantidad_cremalleras_empaquetadas
-) VALUES (1, 100, 100, 'Rojo', 100, 100);
+    cantidad_cremalleras_empaquetadas,
+    fecha_produccion
+) VALUES (1, 100, 100, 'Rojo', 100, 100, CURRENT_DATE);
 INSERT INTO HistorialProduccion (
     maquina_id,
     cantidad_cintas_tejidas,
     cantidad_cremalleras_fabricadas,
     color_teñido,
     cantidad_cremalleras_teñidas,
-    cantidad_cremalleras_empaquetadas
-) VALUES (3, 15, 12, 'Azul', 12, 12);
+    cantidad_cremalleras_empaquetadas,
+    fecha_produccion
+) VALUES (3, 15, 12, 'Azul', 12, 12, CURRENT_DATE);
 INSERT INTO HistorialProduccion (
     maquina_id,
     cantidad_cintas_tejidas,
     cantidad_cremalleras_fabricadas,
     color_teñido,
     cantidad_cremalleras_teñidas,
-    cantidad_cremalleras_empaquetadas
-) VALUES (4, 4, 4, 'Blanco', 4, 4);
+    cantidad_cremalleras_empaquetadas,
+    fecha_produccion
+) VALUES (4, 4, 4, 'Blanco', 4, 4, CURRENT_DATE);
 INSERT INTO HistorialProduccion (
     maquina_id,
     cantidad_cintas_tejidas,
     cantidad_cremalleras_fabricadas,
     color_teñido,
     cantidad_cremalleras_teñidas,
-    cantidad_cremalleras_empaquetadas
-) VALUES (2, 15000, 15000, 'Verde', 15000, 15000);
+    cantidad_cremalleras_empaquetadas,
+    fecha_produccion
+) VALUES (2, 15000, 15000, 'Verde', 15000, 15000, CURRENT_DATE);
 INSERT INTO HistorialProduccion (
     maquina_id,
     cantidad_cintas_tejidas,
     cantidad_cremalleras_fabricadas,
     color_teñido,
     cantidad_cremalleras_teñidas,
-    cantidad_cremalleras_empaquetadas
-) VALUES (5, 1, 1, 'Negro', 1, 1);
+    cantidad_cremalleras_empaquetadas,
+    fecha_produccion
+) VALUES (5, 1, 1, 'Negro', 1, 1, CURRENT_DATE);
